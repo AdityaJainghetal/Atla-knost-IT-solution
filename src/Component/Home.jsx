6afe12c17fa7d5de334ba43e66cf29f3fac30ef2
atlaknotssolutions <atlaknotssolutions@gmail.com>
2026-04-01 11:29:38 +0530

    task

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -695,6 +695,7 @@ export default function Home() {
         />
       </div>
 
+
       <div className="fixed inset-0 pointer-events-none z-0">
         <div className="absolute top-20 right-0 w-96 h-96 bg-red-600/20 rounded-full blur-3xl animate-pulse" />
         <div className="absolute bottom-40 left-0 w-96 h-96 bg-red-800/15 rounded-full blur-3xl animate-pulse delay-1000" />
